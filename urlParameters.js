2386901789f6e054c9894c5e0495b7f6539c8182
playbenz <playbenz@gmail.com>
2019-08-26 12:34:41 +0800

    Update urlParameters.js

--- urlParameters.js
+++ urlParameters.js
@@ -14,9 +14,36 @@ function getdomain(str) {
 	if(str==null)
 		return null;
 	
-	str = str.match(/\/\/(\S*)\//);
+	str = str.parseUrl(str);
 	if(str==null)
 		return null;
 	
-    return str[1].toLowerCase();
+    return str['host'].toLowerCase();
 }
+
+var r = {
+    protocol: /([^\/]+:)\/\/(.*)/i,
+    host: /(^[^\:\/]+)((?:\/|:|$)?.*)/,
+    port: /\:?([^\/]*)(\/?.*)/,
+    pathname: /([^\?#]+)(\??[^#]*)(#?.*)/
+};
+ 
+function parseUrl(url) {
+    var tmp, res = {};
+    res["href"] = url;
+    for (p in r) {
+        tmp = r[p].exec(url);
+        res[p] = tmp[1];
+        url = tmp[2];
+        if (url === "") {
+            url = "/";
+        }
+        if (p === "pathname") {
+            res["pathname"] = tmp[1];
+            res["search"] = tmp[2];
+            res["hash"] = tmp[3];
+        }
+    }
+    console.log(url);
+    return res;
+};
